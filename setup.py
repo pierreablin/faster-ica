0a08656e96660ccc9994d7a96516214cc01e78ff
Pierre Ablin <pierreablin@gmail.com>
2017-08-09 14:55:17 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+import setuptools  # noqa; we are using a setuptools namespace
+from numpy.distutils.core import setup
+
+descr = """Maximum likelihood algorithms"""
+
+DISTNAME = 'fasterica'
+DESCRIPTION = descr
+MAINTAINER = 'Pierre Ablin'
+MAINTAINER_EMAIL = 'pierre.ablin@inria.fr'
+LICENSE = 'BSD (3-clause)'
+DOWNLOAD_URL = 'https://github.com/pierreablin/faster-ica.git'
+VERSION = '0.1.dev0'
+
+if __name__ == "__main__":
+    setup(name=DISTNAME,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          license=LICENSE,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          long_description=open('README.rst').read(),
+          classifiers=[
+              'Intended Audience :: Science/Research',
+              'Intended Audience :: Developers',
+              'License :: OSI Approved',
+              'Programming Language :: Python',
+              'Topic :: Software Development',
+              'Topic :: Scientific/Engineering',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX',
+              'Operating System :: Unix',
+              'Operating System :: MacOS',
+          ],
+          platforms='any',
+          packages=[
+              'picard',
+              'simple_quasi_newton_ica',
+              'truncated_newton',
+              'trust_region'
+          ],
+          )
