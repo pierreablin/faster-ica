0095bcac9051d3733b0171f78a8c28e4dfd0e49a
Pierre Ablin <pierreablin@gmail.com>
2017-08-09 18:24:58 +0200

    update

--- ml_ica/__init__.py
+++ ml_ica/__init__.py
@@ -1,5 +1,5 @@
-from .algorithms import (picard, simple_quasi_newton_ica, truncated_ica,
-                         trust_region_ica)
+from algorithms import (picard, simple_quasi_newton_ica, truncated_ica,
+                        trust_region_ica)
 
 __all__ = ['picard',
            'simple_quasi_newton_ica',
